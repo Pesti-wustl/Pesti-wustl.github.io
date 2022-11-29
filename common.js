13042470db657dd3a841766dd7fdd52a4818f988
pesti-wustl <a.pesti@wustl.edu>
2022-11-28 23:21:30 -0600

    fin

---
+++ common.js
@@ -0,0 +1,26 @@
+
+let button1 = document.getElementById('Submit-1')
+let button2 = document.getElementById('Submit-2')
+let button3 = document.getElementById('Submit-3')
+let button4 = document.getElementById('Submit-4')
+let button5 = document.getElementById('Submit-5')
+
+button1.addEventListener('click', function() {
+    document.querySelector('.Submit-1').style.visibility = 'visible'; 
+})
+
+button2.addEventListener('click', function() {
+    document.querySelector('.Submit-2').style.visibility = 'visible'; 
+})
+
+button3.addEventListener('click', function() {
+    document.querySelector('.Submit-3').style.visibility = 'visible'; 
+})
+
+button4.addEventListener('click', function() {
+    document.querySelector('.Submit-4').style.visibility = 'visible'; 
+})
+
+button5.addEventListener('click', function() {
+    document.querySelector('.Submit-5').style.visibility = 'visible'; 
+})
